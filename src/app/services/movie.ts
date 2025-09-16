a73e60af76bea3dd0d38e73bc2376592bfb8a70a
Kotha107 <fatemakotha17@gmail.com>
2025-09-16 13:27:49 +0600

    updating service

--- src/app/services/movie.ts
+++ src/app/services/movie.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from '@angular/core';
 import { delay, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ApiResult, MovieResult } from '../model/interfaces';
+import { getMovieDetailsURL, getTopRatedMoviesURL } from './endpoints';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY= environment.apiKey;
@@ -15,12 +16,13 @@ export class Movie {
   constructor(){}
   
   getTopRatedMovies(page=1):Observable<ApiResult>{
-    return this.http.get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`)
-    .pipe(delay(5000));
+    return this.http.get<ApiResult>(getTopRatedMoviesURL(page)).pipe(
+      delay(1000)
+    );
 
   }
   getMovieDetails(id : string):Observable<MovieResult>{
-    return this.http.get<MovieResult>(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`);
+    return this.http.get<MovieResult>(getMovieDetailsURL(id));
 
   }
 }
